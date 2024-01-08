57ac1fa88042df03b505694ac4213263cbbde1ff
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-01-08 15:45:18 +0300

    MVC Annotations
    @RequestParam
    required=false - чтобы не вылазила ошибка, в случае, когда мы не передали параметры в http запросе

--- src/main/java/org/example/controllers/SecondController.java
+++ src/main/java/org/example/controllers/SecondController.java
@@ -1,4 +1,4 @@
-package org.example.config.controllers;
+package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
